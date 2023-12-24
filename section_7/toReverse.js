eaad9fef636e73ef1a5c022733f03c445c11c8cd
Jo Young <githuborigin@gmail.com>
2023-12-24 14:00:22 +0900

    feat: es14 toReverse 문법 정리

--- section_7/toReverse.js
+++ section_7/toReverse.js
@@ -11,5 +11,8 @@ array6Reversed.push(6);
 
 console.log(array5Reversed, array6Reversed);
 // [ 2, 5, 4, 1, 3, 6 ] [ 2, 5, 4, 1, 3, 6 ]
+
+// 원본 배열은 수정 안됨
 console.log(array5, array6);
 // [ 2, 5, 4, 1, 3, 6 ] [ 3, 1, 4, 5, 2 ]
+
