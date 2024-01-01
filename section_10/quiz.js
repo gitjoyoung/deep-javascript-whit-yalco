9f04e2e9d3348fbeb1b07edf7eea66b0b97212ce
Jo Young <githuborigin@gmail.com>
2024-01-02 00:16:44 +0900

    feat: section10 퀴즈 문제

---
+++ section_10/quiz.js
@@ -0,0 +1,56 @@
+const array = [
+  "모",
+  "걸",
+  "도",
+  "개",
+  "윷",
+  "윷",
+  "걸",
+  "모",
+  "개",
+  "도",
+  "윷",
+  "모",
+  "걸",
+  "도",
+  "개",
+  "윷",
+  "모",
+  "걸",
+  "개",
+  "도",
+];
+
+//  위 배열의 요소들을 중복요소들을 제거하고
+//  가나다순으로 정렬한 뒤 쉼표로 구분하여
+//  출력하는 코드를 작성해보세요.
+
+const array2 = ["apple", "banana", "apple", "orange", "banana", "apple"];
+
+//  위 배열의 요소들이 각각 몇 개씩 들어있는지를 나타내는
+//  Map 인스턴스를 만드는 코드를 작성해보세요.
+
+//  그 중 'apple'은 몇 개인지 출력해보세요.
+
+//  개수가 가장 적은 순으로 출력해보세요.
+
+const array3 = ["apple", "banana", "apple", "orange", "banana", "apple"];
+
+//  위 배열의 요소들이 각각 몇 개씩 들어있는지를 나타내는
+//  Map 인스턴스를 만드는 코드를 작성해보세요.
+
+//  그 중 'apple'은 몇 개인지 출력해보세요.
+
+//  개수가 가장 적은 순으로 출력해보세요.
+
+//  사용 예시
+const iterable = makeIterable(1, 5);
+for (let value of iterable) {
+  console.log(value); // 1, 2, 3, 4, 5
+}
+
+// 사용 예시
+const generator = makeGenerator(1, 5);
+for (let value of generator) {
+  console.log(value); // 1, 2, 3, 4, 5
+}
